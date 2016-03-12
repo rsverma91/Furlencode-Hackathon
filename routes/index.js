e7ca22f33ee6604cd1ce411088894c7613a6f313
unknown <U6035993@U6035993-TPL-A.ten.thomsonreuters.com>
2016-03-12 23:20:09 +0530

    my changes

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 var indexController = require('../controller/indexController');
 var graphController = require('../controller/graphController');
-
+var productController = require('../controller/productController');
 
 router.get('/', indexController);
 router.get('/graph', graphController);
-
+router.get('/product', productController);
 
 module.exports = router;

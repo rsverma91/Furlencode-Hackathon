a26d1a8b770574f96b454604c49732c4d5d69ff7
Shubham Kumar <shubhamdiscover@gmail.com>
2016-03-12 18:38:22 +0530

    graph

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 var indexController = require('../controller/indexController');
+var graphController = require('../controller/graphController');
 
 
 router.get('/', indexController);
+router.get('/graph', graphController);
 
 
 module.exports = router;
